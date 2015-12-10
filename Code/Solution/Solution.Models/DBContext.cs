58fb5d2e6a05a5bb45a149142f7d5705a7fe58e3
何华杰 <huajie.he@chinasofti.com>
2015-12-10 14:33:49 +0800

    first

---
+++ Code/Solution/Solution.Models/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace Solution.Models
+{
+    public class DBContext: DbContext
+    {
+        static DBContext()
+        {
+            Database.SetInitializer<DBContext>(null);
+        }
+
+        public DBContext()
+            : base("Name=SolutionContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+    }
+}
---
+++ Code/Solution/Solution.Web/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Solution.Common;
+
+namespace Solution.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult LoginSystem(FormCollection collection)
+        {
+            ViewBag.Message = string.Empty;
+            if (!Session["VerifyImageCode"].ToString().Equals(collection["inputCheckCode"].Trim()))
+            {
+                ViewBag.Message = "验证码不正确。";
+            }
+
+
+            return View();
+        }
+
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult VerifyImage()
+        {
+            var s1 = new ValidateCode_Style10();
+            string code = string.Empty;
+            byte[] bytes = s1.CreateImage(out code);
+
+            Session["VerifyImageCode"] = code;
+
+            return File(bytes, @"image/jpeg");
+        }
+    }
+}
