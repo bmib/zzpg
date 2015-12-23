fc7f13bd03d7c54760eb2e60d337bc542c03f0a2
何华杰 <huajie.he@chinasofti.com>
2015-12-23 18:30:38 +0800

    modify：修改菜单项

---
+++ Code/Solution/Solution.Web/Controllers/AssessmentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Solution.Web.Controllers
+{
+    public class AssessmentController : BaseController
+    {
+        //
+        // GET: /Assessment/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ Code/Solution/Solution.Web/Controllers/ManageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Solution.Web.Controllers
+{
+    public class ManageController : BaseController
+    {
+        //
+        // GET: /Manage/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
